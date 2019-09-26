3ae70deba8c44e02eb096f6bdb65550a4aae467f
Jason Crawford <daxapps777@gmail.com>
2019-09-26 10:57:05 -0400

    basic screen nav setup

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
 import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const trackListFlow = createStackNavigator({
+	TrackList: TrackListScreen,
+	TrackDetail: TrackDetailScreen
+});
+
+const switchNavigator = createSwitchNavigator({
+	// ResolveAuth: ResolveAuthScreen,
+	loginFlow: createStackNavigator({
+		Signup: SignupScreen,
+		Signin: SigninScreen
+	}),
+	mainFlow: createBottomTabNavigator({
+		trackListFlow,
+		TrackCreate: TrackCreateScreen,
+		Account: AccountScreen
+	})
+});
+
+export default createAppContainer(switchNavigator);
+
+// const App = createAppContainer(switchNavigator);
+
+// export default () => {
+// 	return (
+// 		<TrackProvider>
+// 			<LocationProvider>
+// 				<AuthProvider>
+// 					<App
+// 						ref={(navigator) => {
+// 							setNavigator(navigator);
+// 						}}
+// 					/>
+// 				</AuthProvider>
+// 			</LocationProvider>
+// 		</TrackProvider>
+// 	);
+// };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
