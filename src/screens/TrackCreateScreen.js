ae054211993ea9cd18d972e12a6bad1b8d21a8a3
Jason Crawford <daxapps777@gmail.com>
2019-10-05 07:22:28 -0400

    display track details

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,13 @@
-import '../_mockLocation';
+// import '../_mockLocation';
 import React, { useContext, useCallback } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView, withNavigationFocus } from 'react-navigation';
-// import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 import TrackForm from '../components/TrackForm';
-// import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 const TrackCreateScreen = ({ isFocused }) => {
 	const { state: { recording }, addLocation } = useContext(LocationContext);
@@ -30,10 +29,10 @@ const TrackCreateScreen = ({ isFocused }) => {
 	);
 };
 
-// TrackCreateScreen.navigationOptions = {
-// 	title: 'Add Track',
-// 	tabBarIcon: <FontAwesome name="plus" size={20} />
-// };
+TrackCreateScreen.navigationOptions = {
+	title: 'Add Track',
+	tabBarIcon: <FontAwesome name="plus" size={20} />
+};
 
 const styles = StyleSheet.create({});
 
